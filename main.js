03cb1b4046e53038d3ea36a1712b0f29d8927d39
christinamelnikova <130339715+christinamelnikova@users.noreply.github.com>
2023-09-16 15:57:15 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,30 +1,35 @@
 let container = document.querySelector('.form')
-let data = document.querySelectorAll('.inputData')
-let saveButton = document.querySelector('.saveBtn')
-let openButton = document.querySelector('.openBtn')
-
-
-if (localStorage.getItem("default")){
-   container.innerHTML = localStorage.getItem("default");
+function newInput(){
+    let data = document.querySelectorAll('.inputData')
+    data.forEach(inp =>{
+        inp.addEventListener('input', function(){
+            if (inp.tagName === 'INPUT')
+                inp.setAttribute("value", inp.value);
+            if (inp.tagName === 'TEXTAREA')
+                inp.innerText = inp.value;
+            localStorage.setItem("default", container.innerHTML)
+        })
+    })
 }
 
-data.forEach(inp =>{
-    inp.addEventListener('input', function(){
-        if (inp.tagName === 'INPUT')
-            inp.setAttribute("value", inp.value);
-        if (inp.tagName === 'TEXTAREA')
-            inp.innerText = inp.value;
-        localStorage.setItem("default", container.innerHTML)
-    })
-})
+if (localStorage.getItem("default")){
+  container.innerHTML = localStorage.getItem("default");
+  newInput()
+}
+
+const saveButton = document.querySelector('.saveBtn')
 
 saveButton.addEventListener('click', function(){
     let name = prompt('Как Вас зовут?')
     localStorage.setItem(name, container.innerHTML)
 })
 
+const openButton = document.querySelector('.openBtn')
+
 openButton.addEventListener('click', function (){
     let resume = prompt('Какое резюме открыть?')
-    if (localStorage.getItem(resume))
+    if (localStorage.getItem(resume)){
         container.innerHTML = localStorage.getItem(resume)
+        newInput()
+    }
 })
